be31d97cac76407ecb5a0934a7f66022bc583266
Vladimir Borovic <vladimir.borovic@gmail.com>
2013-10-04 10:46:54 +0300

    create entity BranchTitle

---
+++ src/main/java/md/varoinform/model/entities/BranchTitle.java
@@ -0,0 +1,25 @@
+package md.varoinform.model.entities;
+
+import javax.persistence.*;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Vladimir Borovic
+ * Date: 10/4/13
+ * Time: 10:42 AM
+ */
+@Entity
+@Table(name = "DB_branchtitle")
+public class BranchTitle extends Title{
+    private Branch branch;
+
+    @ManyToOne
+    @JoinColumn(name = "id")
+    public Branch getBranch() {
+        return branch;
+    }
+
+    public void setBranch(Branch branch) {
+        this.branch = branch;
+    }
+}
