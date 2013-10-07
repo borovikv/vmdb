3bd7b8149dc790f0544b8ef426e4d11a7a2f3e35
Vladimir Borovic <vladimir.borovic@gmail.com>
2013-10-07 12:20:20 +0300

    create Contact stub

---
+++ src/main/java/md/varoinform/model/entities/Contact.java
@@ -0,0 +1,15 @@
+package md.varoinform.model.entities;
+
+import javax.persistence.*;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Vladimir Borovic
+ * Date: 10/7/13
+ * Time: 12:04 PM
+ */
+//ToDo:create Contact
+@Entity
+@Table(name = "DB_contact")
+public class Contact {
+}
