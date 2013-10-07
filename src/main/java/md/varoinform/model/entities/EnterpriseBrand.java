b23701d6deeb594bd2b8909798d3d3fee60e13fe
Vladimir Borovic <vladimir.borovic@gmail.com>
2013-10-07 11:17:47 +0300

    create EnterpriseBrand

---
+++ src/main/java/md/varoinform/model/entities/EnterpriseBrand.java
@@ -0,0 +1,51 @@
+package md.varoinform.model.entities;
+
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Vladimir Borovic
+ * Date: 10/7/13
+ * Time: 11:11 AM
+ */
+@Entity
+@Table(name = "DB_enterprise_brand")
+public class EnterpriseBrand {
+    private Long id;
+    private Enterprise enterprise;
+    private Brand brand;
+
+    @Id
+    @GeneratedValue(generator = "increment")
+    @GenericGenerator(name = "increment", strategy = "increment")
+    @Column(name = "id")
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @ManyToOne
+    @JoinColumn(name = "enterprise_id")
+    public Enterprise getEnterprise() {
+        return enterprise;
+    }
+
+    public void setEnterprise(Enterprise enterprise) {
+        this.enterprise = enterprise;
+    }
+
+    @ManyToOne
+    @JoinColumn(name = "brand_id")
+    public Brand getBrand() {
+        return brand;
+    }
+
+    public void setBrand(Brand brand) {
+        this.brand = brand;
+    }
+}
