97bbe468b6eb6f7dcd3e6447446141b80ac6e594
Vladimir Borovic <vladimir.borovic@gmail.com>
2013-10-07 13:48:41 +0300

    added Town + TownTitle

---
+++ src/main/java/md/varoinform/model/entities/Town.java
@@ -0,0 +1,9 @@
+package md.varoinform.model.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "DB_town")
+public class Town extends TitleContainer {
+}
---
+++ src/main/java/md/varoinform/model/entities/TownTitle.java
@@ -0,0 +1,26 @@
+package md.varoinform.model.entities;
+
+import javax.persistence.*;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Vladimir Borovic
+ * Date: 10/7/13
+ * Time: 1:47 PM
+ */
+@Entity
+@Table(name = "DB_towntitle")
+public class TownTitle extends Title {
+    private Town town;
+
+    @ManyToOne
+    @JoinColumn(name = "town_id")
+    public Town getTown() {
+        return town;
+    }
+
+    public void setTown(Town town) {
+        this.town = town;
+    }
+}
+
