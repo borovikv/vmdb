649001235c9b6a61695e8bc304caad9717318d7d
Vladimir Borovic <vladimir.borovic@gmail.com>
2013-10-07 11:09:56 +0300

    create Brand

---
+++ src/main/java/md/varoinform/model/entities/Brand.java
@@ -0,0 +1,37 @@
+package md.varoinform.model.entities;
+
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Vladimir Borovic
+ * Date: 10/7/13
+ * Time: 11:05 AM
+ */
+public class Brand {
+    private Long id;
+    private String title;
+
+    @Id
+    @GeneratedValue(generator = "increment")
+    @GenericGenerator(name = "increment", strategy = "increment")
+    @Column(name = "id")
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Column(name = "title")
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
