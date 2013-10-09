e1f2b3fd63ff7ff9315b9b63c00351217678e4dc
Vladimir Borovic <vladimir.borovic@gmail.com>
2013-10-09 16:36:38 +0300

    create SearchTest

---
+++ src/test/java/md/varoinform/modeltest/searchengine/SearchTest.java
@@ -0,0 +1,13 @@
+package md.varoinform.modeltest.searchengine;
+
+import org.junit.*;
+import static org.junit.Assert.*;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Vladimir Borovic
+ * Date: 10/9/13
+ * Time: 4:33 PM
+ */
+public class SearchTest {
+}
