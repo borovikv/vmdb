e9620f3c173ad20fdf90eeaa947e5bce8e7a782e
Vladimir Borovic <vladimir.borovic@gmail.com>
2013-10-24 10:19:32 +0300

    creating test for createEnterprises

--- src/test/java/md/varoinform/modeltest/searchengine/SearchTest.java
+++ src/test/java/md/varoinform/modeltest/searchengine/SearchTest.java
@@ -1,6 +1,12 @@
 package md.varoinform.modeltest.searchengine;
 
+import md.varoinform.model.entities.Enterprise;
+import md.varoinform.modeltest.TestHibernateBase;
+import md.varoinform.modeltest.util.EntityCreator;
 import org.junit.*;
+
+import java.util.List;
+
 import static org.junit.Assert.*;
 
 /**
@@ -9,5 +15,20 @@ import static org.junit.Assert.*;
  * Date: 10/9/13
  * Time: 4:33 PM
  */
-public class SearchTest {
+public class SearchTest extends TestHibernateBase {
+    List<Enterprise> enterprises;
+
+    @Before
+    public void createEnterprises(){
+        enterprises = EntityCreator.createEnterprises();
+        for (Enterprise enterprise : enterprises) {
+            session.refresh(enterprise);
+            System.out.println(enterprise);
+        }
+    }
+
+    @Test
+    public void testIsCreated(){
+        assertEquals(enterprises.size(), 3);
+    }
 }
