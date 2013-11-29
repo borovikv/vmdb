609b9b5c3f445819691a3f2af1ffe84607082d69
Vladimir Borovic <vladimir.borovic@gmail.com>
2013-11-29 09:37:47 +0200

    Mail end;

---
+++ src/main/java/md/varoinform/controller/MailProxy.java
@@ -0,0 +1,46 @@
+package md.varoinform.controller;
+
+import md.varoinform.controller.entityproxy.EnterpriseProxy;
+import md.varoinform.model.entities.Enterprise;
+
+import java.awt.*;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.List;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Vladimir Borovic
+ * Date: 11/28/13
+ * Time: 4:00 PM
+ */
+public class MailProxy {
+    private List<Enterprise> enterprises;
+
+    public MailProxy(List<Enterprise> enterprises) {
+        this.enterprises = enterprises;
+    }
+
+    public void mail(){
+        Desktop desktop;
+        if (Desktop.isDesktopSupported() && (desktop = Desktop.getDesktop()).isSupported(Desktop.Action.MAIL)){
+            try {
+                URI uri = new URI(getMailTo());
+                desktop.mail(uri);
+            } catch (IOException | URISyntaxException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private String getMailTo() {
+        StringBuilder builder = new StringBuilder("mailto:vladimir@varo-inform.com, borovikv.vladimir@gmail.com");
+        for (Enterprise enterprise : enterprises) {
+            EnterpriseProxy proxy = new EnterpriseProxy(enterprise);
+            builder.append(proxy.getEmails());
+            builder.append(",");
+        }
+        return builder.toString().replaceAll("\\s*", "");
+    }
+}
