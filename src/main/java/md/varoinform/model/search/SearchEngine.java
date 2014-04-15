a0b45022eca7fede63019a9a9ff26101ab5f359f
Vladimir Borovic <vladimir.borovic@gmail.com>
2014-04-15 10:56:24 +0300

    implements new search strategy

--- src/main/java/md/varoinform/model/search/SearchEngine.java
+++ src/main/java/md/varoinform/model/search/SearchEngine.java
@@ -7,6 +7,7 @@ import org.hibernate.Session;
 import org.hibernate.Transaction;
 import org.hibernate.search.FullTextSession;
 import org.hibernate.search.Search;
+import org.hibernate.search.query.dsl.PhraseMatchingContext;
 import org.hibernate.search.query.dsl.QueryBuilder;
 
 import java.util.List;
@@ -74,7 +75,7 @@ public class SearchEngine {
         // alternatively you can write the Lucene query using the Lucene query parser
         // or the Lucene programmatic API. The Hibernate Search DSL is recommended though
         QueryBuilder qb = fullTextSession.getSearchFactory().buildQueryBuilder().forEntity(Enterprise.class).get();
-        return qb.keyword()
+        /*return qb.keyword().fuzzy().withThreshold(0.8f).withPrefixLength(1)
                 .onFields("titles.title", "goods.good.titles.title", "goods.good.treeNodes.title.titles.title", "brands.title",
                         "contacts.postalCode", "contacts.houseNumber", "contacts.officeNumber",
                         "contacts.street.titles.title", "contacts.sector.titles.title", "contacts.town.titles.title",
@@ -83,5 +84,18 @@ public class SearchEngine {
                         "contactPersons.person.titles.title", "contactPersons.phones.phone")
                 .matching(q)
                 .createQuery();
+        */
+        String[] strings = {"goods.good.titles.title", "goods.good.treeNodes.title.titles.title", "brands.title",
+                "contacts.postalCode", "contacts.houseNumber", "contacts.officeNumber",
+                "contacts.street.titles.title", "contacts.sector.titles.title", "contacts.town.titles.title",
+                "contacts.region.titles.title",
+                "contacts.emails.email", "contacts.phones.phone", "contacts.urls.url",
+                "contactPersons.person.titles.title", "contactPersons.phones.phone"};
+        PhraseMatchingContext phraseMatchingContext = qb.phrase().onField("titles.title");
+        for (String string : strings) {
+            phraseMatchingContext.andField(string);
+        }
+        return phraseMatchingContext.sentence(q).createQuery();
+
     }
 }
