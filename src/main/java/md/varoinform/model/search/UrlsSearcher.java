d62483237c6d11cd1e2c96920808a3457c6f8926
Vladimir Borovic <vladimir.borovic@gmail.com>
2014-04-25 14:01:40 +0300

    search URL

--- src/main/java/md/varoinform/model/search/UrlsSearcher.java
+++ src/main/java/md/varoinform/model/search/UrlsSearcher.java
@@ -1,6 +1,8 @@
 package md.varoinform.model.search;
 
 import md.varoinform.model.entities.Enterprise;
+import md.varoinform.model.util.SessionManager;
+import org.hibernate.Query;
 
 import java.util.List;
 
@@ -13,6 +15,9 @@ import java.util.List;
 public class UrlsSearcher extends Searcher {
     @Override
     public List<Enterprise> search(String q) {
-        return null;
+        String hql = "Select distinct e from Enterprise e join e.contacts cs join cs.urls urls where lower(urls.url) like :pattern";
+        Query query = SessionManager.getSession().createQuery(hql).setString("pattern", "%" + q + "%");
+        //noinspection unchecked
+        return query.list();
     }
 }
