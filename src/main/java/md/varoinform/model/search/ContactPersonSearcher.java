81986b2f889eb9e25963396588b8e72204d210d4
Vladimir Borovic <vladimir.borovic@gmail.com>
2014-04-25 15:07:20 +0300

    search ContactPerson

--- src/main/java/md/varoinform/model/search/ContactPersonSearcher.java
+++ src/main/java/md/varoinform/model/search/ContactPersonSearcher.java
@@ -1,6 +1,8 @@
 package md.varoinform.model.search;
 
 import md.varoinform.model.entities.Enterprise;
+import md.varoinform.model.util.SessionManager;
+import org.hibernate.Query;
 
 import java.util.List;
 
@@ -13,7 +15,10 @@ import java.util.List;
 public class ContactPersonSearcher extends Searcher {
     @Override
     public List<Enterprise> search(String q) {
-        return null;
+        String hql = "Select distinct e from Enterprise e join e.contactPersons cp join cp.person p join p.titles t where lower(t.title) like :pattern";
+        Query query = SessionManager.getSession().createQuery(hql).setString("pattern", "%" + q.toLowerCase() + "%");
+        //noinspection unchecked
+        return query.list();
     }
 
 
