703df9c4b7f79efe9d10c7387d5339e89804799e
Vladimir Borovic <vladimir.borovic@gmail.com>
2014-04-25 12:00:07 +0300

    search Workplaces

--- src/main/java/md/varoinform/model/search/WorkplacesSearcher.java
+++ src/main/java/md/varoinform/model/search/WorkplacesSearcher.java
@@ -1,6 +1,8 @@
 package md.varoinform.model.search;
 
 import md.varoinform.model.entities.Enterprise;
+import md.varoinform.model.util.SessionManager;
+import org.hibernate.Query;
 
 import java.util.List;
 
@@ -11,9 +13,37 @@ import java.util.List;
  * Time: 10:22 AM
  */
 public class WorkplacesSearcher extends Searcher {
-    @Override
-    public List<Enterprise> search(String q) {
-        return null;
+    private final ComparisonType type;
+
+    public static enum ComparisonType {
+        GTE(">"), LTE("<"), EQ("=");
+        private final String operator;
+
+        ComparisonType(String operator) {
+            this.operator = operator;
+        }
+
+        @Override
+        public String toString() {
+            return operator;
+        }
     }
 
+    public WorkplacesSearcher(ComparisonType type ) {
+        this.type = type;
+    }
+
+    @Override
+    public List<Enterprise> search(String q) {
+        Integer amount = Integer.parseInt(q);
+        String hql = "Select distinct e from Enterprise e where e.workplaces " + type + " :amount";
+        Query query = SessionManager.getSession().createQuery(hql).setInteger("amount", amount);
+        //noinspection unchecked
+        return query.list();
+    }
+
+    @Override
+    public String getName() {
+        return super.getName() + type.toString();
+    }
 }
