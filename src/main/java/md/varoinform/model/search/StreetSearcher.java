92f2f68f05e623e92aa676aad1e9dde552566b7a
Vladimir Borovic <vladimir.borovic@gmail.com>
2014-04-25 14:22:02 +0300

    search Street

--- src/main/java/md/varoinform/model/search/StreetSearcher.java
+++ src/main/java/md/varoinform/model/search/StreetSearcher.java
@@ -1,6 +1,8 @@
 package md.varoinform.model.search;
 
 import md.varoinform.model.entities.Enterprise;
+import md.varoinform.model.util.SessionManager;
+import org.hibernate.Query;
 
 import java.util.List;
 
@@ -13,7 +15,10 @@ import java.util.List;
 public class StreetSearcher extends Searcher {
     @Override
     public List<Enterprise> search(String q) {
-        return null;
+        String hql = "Select distinct e from Enterprise e join e.contacts cs join cs.street s join s.titles titles where lower(titles.title) like :pattern";
+        Query query = SessionManager.getSession().createQuery(hql).setString("pattern", q);
+        //noinspection unchecked
+        return query.list();
     }
 
 }
