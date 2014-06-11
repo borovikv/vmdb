8aae70d524c52d2ae8e660dc2d6e0e2d5dfdee54
Vladimir Borovic <vladimir.borovic@gmail.com>
2014-06-11 10:05:02 +0300

    fixing bug history

--- src/main/java/md/varoinform/view/navigation/search/SearchPanel.java
+++ src/main/java/md/varoinform/view/navigation/search/SearchPanel.java
@@ -36,17 +36,7 @@ public class SearchPanel implements Observer {
         ActionListener searchAction = new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                List<Enterprise> enterprises = ActivityDialog.start(new SwingWorker<List<Enterprise>, Object>() {
-                    @Override
-                    protected List<Enterprise> doInBackground() throws Exception {
-                        Searcher searcher = searcherCombo.getSearcher();
-                        return searchText(searcher, searchField.getText());
-                    }
-                }, ResourceBundleHelper.getString("search-wait-dialog-message", "Wait..."));
-
-                for (SearchListener listener : listeners) {
-                    listener.perform(enterprises);
-                }
+                search(searcherCombo.getSearcher(), searchField.getText());
             }
         };
 
@@ -59,6 +49,18 @@ public class SearchPanel implements Observer {
         searcherCombo = new FieldSearcherCombo(Searchers.getSearchers());
 
         History.instance.addObserver(this);
+    }
+
+    public void search(Searcher searcher, String text) {
+        String message = ResourceBundleHelper.getString("search-wait-dialog-message", "Wait...");
+        List<Enterprise> enterprises = ActivityDialog.start(new SearchWorker(searcher, text), message);
+        fireSearchEnded(enterprises);
+    }
+
+    public void fireSearchEnded(List<Enterprise> enterprises) {
+        for (SearchListener listener : listeners) {
+            listener.perform(enterprises);
+        }
     }
 
     private List<Enterprise> searchText(Searcher searcher, String text) {
@@ -101,7 +103,7 @@ public class SearchPanel implements Observer {
             if (source instanceof Searcher){
                 searcherCombo.setSearcher((Searcher) source);
                 String text = (String) ((HistoryEvent) value).getState();
-                searchText((Searcher) source, text);
+                search((Searcher) source, text);
                 searchField.setText(text);
             } else {
                 searchField.setText("");
@@ -109,5 +111,21 @@ public class SearchPanel implements Observer {
             }
         }
 
+    }
+
+    private class SearchWorker extends SwingWorker<List<Enterprise>, Object> {
+
+        private Searcher searcher;
+        private String text;
+
+        private SearchWorker(Searcher searcher, String text) {
+            this.searcher = searcher;
+            this.text = text;
+        }
+
+        @Override
+        protected List<Enterprise> doInBackground() throws Exception {
+            return searchText(searcher, text);
+        }
     }
 }
