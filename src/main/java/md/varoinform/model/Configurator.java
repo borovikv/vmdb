b01e7865e195aa649a9995edb08ed08e216f3d84
Vladimir Borovic <vladimir.borovic@gmail.com>
2014-07-07 10:20:04 +0300

    refactoring configurator

--- src/main/java/md/varoinform/model/Configurator.java
+++ src/main/java/md/varoinform/model/Configurator.java
@@ -17,6 +17,29 @@ import java.nio.file.Paths;
  * Time: 3:51 PM
  */
 public class Configurator {
+    private static final Class[] entities = {
+            Advertisement.class, AdvertisementText.class,
+            Arc.class, Node.class, NodeTitle.class,
+            Good.class, GoodTitle.class, GProduce.class,
+            Brand.class,
+            BusinessEntityType.class, BusinessEntityTypeTitle.class,
+            Contact.class,
+            ContactPerson.class,
+            Email.class,
+            Enterprise.class, EnterpriseTitle.class,
+            Language.class,
+            Person.class, PersonTitle.class,
+            Phone.class,
+            Position.class, PositionTitle.class,
+            Region.class, RegionTitle.class,
+            Sector.class, SectorTitle.class,
+            Street.class, StreetTitle.class,
+            Tag.class, TagEnterprise.class,
+            Database.class,
+            TopAdministrativeUnit.class, TopAdministrativeUnitTitle.class,
+            Town.class, TownTitle.class,
+            Url.class
+    };
     private final String pathToDb;
     private final String password;
 
@@ -28,7 +51,8 @@ public class Configurator {
     public Configurator(String pathToDb) {
         if (pathToDb == null || pathToDb.isEmpty()) throw new IllegalArgumentException();
         Path path = Paths.get(pathToDb);
-        if (Files.notExists(path.getParent())) throw new RuntimeException("file " + path.toAbsolutePath().toString() + " not found");
+        if (Files.notExists(path.getParent()))
+            throw new RuntimeException("file " + path.toAbsolutePath().toString() + " not found");
         this.pathToDb = path.toString();
         password = PasswordManager.getPassword();
     }
@@ -41,58 +65,25 @@ public class Configurator {
     }
 
     public Configuration getConfiguration() {
-        Configuration cfg  = new Configuration();
-        cfg.addAnnotatedClass(Advertisement.class);
-        cfg.addAnnotatedClass(AdvertisementText.class);
-        cfg.addAnnotatedClass(Arc.class);
-        cfg.addAnnotatedClass(Node.class);
-        cfg.addAnnotatedClass(NodeTitle.class);
+        Configuration cfg = new Configuration();
 
-        cfg.addAnnotatedClass(Good.class);
-        cfg.addAnnotatedClass(GoodTitle.class);
-        cfg.addAnnotatedClass(GProduce.class);
-
-        cfg.addAnnotatedClass(Brand.class);
-        cfg.addAnnotatedClass(BusinessEntityType.class);
-        cfg.addAnnotatedClass(BusinessEntityTypeTitle.class);
-        cfg.addAnnotatedClass(Contact.class);
-        cfg.addAnnotatedClass(ContactPerson.class);
-        cfg.addAnnotatedClass(Email.class);
-        cfg.addAnnotatedClass(Enterprise.class);
-        cfg.addAnnotatedClass(EnterpriseTitle.class);
-        cfg.addAnnotatedClass(Language.class);
-        cfg.addAnnotatedClass(Person.class);
-        cfg.addAnnotatedClass(PersonTitle.class);
-        cfg.addAnnotatedClass(Phone.class);
-        cfg.addAnnotatedClass(Position.class);
-        cfg.addAnnotatedClass(PositionTitle.class);
-        cfg.addAnnotatedClass(Region.class);
-        cfg.addAnnotatedClass(RegionTitle.class);
-        cfg.addAnnotatedClass(Sector.class);
-        cfg.addAnnotatedClass(SectorTitle.class);
-        cfg.addAnnotatedClass(Street.class);
-        cfg.addAnnotatedClass(StreetTitle.class);
-        cfg.addAnnotatedClass(Tag.class);
-        cfg.addAnnotatedClass(TagEnterprise.class);
-        cfg.addAnnotatedClass(Database.class);
-        cfg.addAnnotatedClass(TopAdministrativeUnit.class);
-        cfg.addAnnotatedClass(TopAdministrativeUnitTitle.class);
-        cfg.addAnnotatedClass(Town.class);
-        cfg.addAnnotatedClass(TownTitle.class);
-        cfg.addAnnotatedClass(Url.class);
+        for (Class entity : entities) {
+            cfg.addAnnotatedClass(entity);
+        }
 
         cfg.setProperty("hibernate.connection.driver_class", "org.h2.Driver");
+        cfg.setProperty("hibernate.dialect", "org.hibernate.dialect.H2Dialect");
 
         cfg.setProperty("hibernate.connection.url", "jdbc:h2:file:" + pathToDb);
         cfg.setProperty("hibernate.connection.username", "admin");
         cfg.setProperty("hibernate.connection.password", password);
+
         cfg.setProperty("hibernate.connection.pool_size", "1");
-        cfg.setProperty("hibernate.dialect", "org.hibernate.dialect.H2Dialect");
+        cfg.setProperty("hibernate.current_session_context_class", "thread");
+
         cfg.setProperty("hibernate.generate_statistics", "false");
         cfg.setProperty("hibernate.use_sql_comments", "false");
         cfg.setProperty("hibernate.connection.autocommit", "false");
-        cfg.setProperty("hibernate.current_session_context_class", "thread");
-        //cfg.setProperty("hibernate.cache.provider_class", "org.hibernate.cache.internal.NoCacheProvider");
 
         cfg.setProperty("hibernate.search.default.directory_provider", "filesystem");
         cfg.setProperty("hibernate.search.default.indexBase", pathToDb + "/indexes");
@@ -100,11 +91,11 @@ public class Configurator {
     }
 
     @SuppressWarnings("UnusedDeclaration")
-    public void setAuto(Configuration cfg, String value){
+    public void setAuto(Configuration cfg, String value) {
         cfg.setProperty("hibernate.hbm2ddl.auto", value);
     }
 
-    public void showSql(Configuration cfg, boolean b){
+    public void showSql(Configuration cfg, boolean b) {
         cfg.setProperty("hibernate.show_sql", Boolean.toString(b));
 
     }
