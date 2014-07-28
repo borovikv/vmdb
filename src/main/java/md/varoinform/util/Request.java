da4c6565512a39ff3b041eadc209e82a314f50f6
Vladimir Borovic <vladimir.borovic@gmail.com>
2014-07-28 16:25:56 +0300

    added get through proxy to Request

--- src/main/java/md/varoinform/util/Request.java
+++ src/main/java/md/varoinform/util/Request.java
@@ -1,22 +1,64 @@
 package md.varoinform.util;
 
+import org.apache.commons.io.IOUtils;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.fluent.Response;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.*;
 
-//ToDo: Proxy
 public class Request {
 
     private String url;
+    private final PreferencesHelper helper;
 
     public Request(String url) {
         this.url = url;
+        helper = new PreferencesHelper();
     }
 
     public String timesGet(int times) throws IOException {
         if (times <= 0) return null;
+
+        if (!helper.getUseProxy()){
+            return getSimple(times);
+        } else {
+            String hostname = helper.getProxyAddress();
+            Integer port = helper.getProxyPort();
+            final String user = helper.getProxyUser();
+            final String password = helper.getProxyPassword();
+            return getViaProxy(times, hostname, port, user, password);
+        }
+    }
+
+    public String getViaProxy(int times, String hostname, int port, final String user, final String password) throws IOException {
+        if (times <= 0) return null;
+
+        Proxy proxy = new Proxy(Proxy.Type.HTTP, new InetSocketAddress(hostname, port));
+        Authenticator authenticator = new Authenticator() {
+            @Override
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(user, password.toCharArray());
+            }
+        };
+        Authenticator.setDefault(authenticator);
+        try {
+            HttpURLConnection connection = (HttpURLConnection) new URL(url).openConnection(proxy);
+            connection.setRequestMethod("GET");
+            connection.connect();
+            InputStream inputStream = connection.getInputStream();
+            return IOUtils.toString(inputStream);
+        } catch (IOException e) {
+            if (times == 1) throw e;
+            return getViaProxy(times - 1, hostname, port, user, password);
+        }
+    }
+
+    public String getSimple(int times) throws IOException {
+        if (times <= 0) return null;
+
         try {
             Response execute = org.apache.http.client.fluent.Request.Get(url).execute();
             HttpResponse response = execute.returnResponse();
@@ -27,7 +69,7 @@ public class Request {
             return out.toString();
         } catch (IOException e) {
             if (times == 1) throw e;
-            return timesGet(times - 1);
+            return getSimple(times - 1);
         }
     }
 
