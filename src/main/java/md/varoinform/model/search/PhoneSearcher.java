253b1c818ffcd5749a368d8f9e2fd6acecb1b035
Vladimir Borovic <vladimir.borovic@gmail.com>
2014-12-03 12:14:51 +0200

    phone search change pattern from %xxx to %xxx%

--- src/main/java/md/varoinform/model/search/PhoneSearcher.java
+++ src/main/java/md/varoinform/model/search/PhoneSearcher.java
@@ -16,7 +16,7 @@ public class PhoneSearcher extends Searcher {
     public List<Long> search(String q) {
         try (ClosableSession session = new ClosableSession()) {
             String hql = "Select distinct e.id from Enterprise e join e.contacts c join c.allPhones p where lower(p.phone) like :pattern";
-            Query query = session.createQuery(hql).setString("pattern", "%" + q.trim().toLowerCase());
+            Query query = session.createQuery(hql).setString("pattern", "%" + q.trim().toLowerCase() + "%");
             //noinspection unchecked
             return query.list();
         }
